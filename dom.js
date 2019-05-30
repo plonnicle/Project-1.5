5db9fb434dbbadd9f5d412d0f16368aeea8af374
plonnicle <51133264+plonnicle@users.noreply.github.com>
2019-05-30 16:52:03 -0400

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,26 @@
+'use strict';
+/*
+// Get element by id in two ways:
+let main_p = document.getElementById("main-text");
+main_p = document.querySelector("#button"); // Uses CSS selector
+
+main_p.innerText = "Example 1"
+
+let counter = 0;
+main_p.addEventListener("click", () => {
+    counter = counter + 1;
+    main_p.innerText = `This button has been clicked: ${counter} times.`;
+});
+
+let button = document.getElementById("button");
+button.addEventListener("mouseover", () => {
+    document.body.style.backgroundColor = "red";
+});*/
+function toggleText() {
+    var text = document.getElementById("demo");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
